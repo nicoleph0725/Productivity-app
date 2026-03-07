ea3603e795cd3b1177ef14843a1f2338b22da006
nicoleph0725 <nicolepham2019@gmail.com>
2026-03-07 18:47:44 +0000

    make task page

--- website/views.py
+++ website/views.py
@@ -6,9 +6,16 @@ import json
 
 main_views = Blueprint('main_views', __name__)
 
+# Home Route
 @main_views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    return render_template("home.html", user=current_user)
+
+# Task page route
+@main_views.route('/task_page', methods=['GET', 'POST'])
+@login_required
+def task_page():
     if request.method == 'POST':
         task = request.form.get('task')
         if len(task) < 1:
@@ -18,8 +25,10 @@ def home():
             db.session.add(new_task)
             db.session.commit()
             flash('Task added', category='success')
-    return render_template("home.html", user=current_user)
+    return render_template("task_page.html", user=current_user)
 
+
+# For deleting tasks, if this is called the task page is returned normally
 @main_views.route('/delete-task', methods=['POST'])
 def delete_task():
     task = json.loads(request.data)
